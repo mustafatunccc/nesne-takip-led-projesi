252ac1b5260f0b35cd7da9a8f1d01da07a29cb48
mustafatunccc <mustafatunc3737@hotmail.com>
2018-12-17 20:07:06 +0300

    Nesne takip led projesi
    Nesne konumunun ledlerle gösterilmesi

---
+++ first_project_in_image_processing/first_project_in_image_processing/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace first_project_in_image_processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 360);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(478, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(450, 360);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(6, 19);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(148, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Çıkış";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(37, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Bağlan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(37, 86);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 36);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Bağlantıyı Kes";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 42);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(128, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Kırmızı Rengi Takip Et";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 65);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(121, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Yeşil Rengi Takip Et";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 88);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(122, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Mavi Rengi Takip Et";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 388);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(175, 137);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kamera Bağlantısı";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(202, 388);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(164, 137);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Arduino Bağlantısı";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Location = new System.Drawing.Point(526, 388);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(140, 137);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Renk Filtreleme";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 537);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Nesne Konumunun Ledlerle Gösterilmesi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ first_project_in_image_processing/first_project_in_image_processing/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.IO.Ports;
+
+namespace first_project_in_image_processing
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection VideoCapTureDevices;   // form1 load içindeki tanımsızlık için yazıldı. Kameradan görüntüalmayla ilgili...
+        VideoCaptureDevice videoSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.DataSource = SerialPort.GetPortNames();
+            VideoCapTureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo VideoCaptureDevice in VideoCapTureDevices)
+
+            {
+
+                comboBox1.Items.Add(VideoCaptureDevice.Name);
+
+            }
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // create video source
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(VideoCapTureDevices[0].MonikerString);
+            // set NewFrame event handler
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            // start the video source
+            videoSource.Start();
+
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // video kameradan alınan görüntüler bitmap formatında alınıyor.
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap image1 = (Bitmap)eventArgs.Frame.Clone();
+
+
+
+            // aynalama yöntemini görüntümüze uyguladık.
+            Mirror mirrorfilter = new Mirror(false, true);
+            // apply the filter
+            mirrorfilter.ApplyInPlace(image);
+
+            // bicubik yöntem ile görüntümüzün boyutlarını belirledik.                                                
+            ResizeBicubic filter = new ResizeBicubic(450, 360);
+            // apply the filter
+            image = filter.Apply(image);
+
+            // aynalama yöntemini görüntümüze uyguladık.
+            Mirror mirrorfilterr = new Mirror(false, true);
+            // apply the filter
+            mirrorfilterr.ApplyInPlace(image1);
+
+            // bicubik yöntem ile görüntümüzün boyutlarını belirledik.                                                
+            ResizeBicubic filterr = new ResizeBicubic(450, 360);
+            // apply the filter
+            image1 = filterr.Apply(image1);
+
+
+            pictureBox1.Image = image;
+
+            if (radioButton1.Checked)
+            {
+
+                // create filter
+                EuclideanColorFiltering oklidfilter = new EuclideanColorFiltering();
+                // set center colol and radius
+                oklidfilter.CenterColor = new RGB(215, 0, 0);
+                oklidfilter.Radius = 100;
+                // apply the filter
+                oklidfilter.ApplyInPlace(image1);
+                nesnebul(image1);
+
+            }
+
+            if (radioButton2.Checked)
+            {
+
+                // create filter
+                EuclideanColorFiltering oklidfilter = new EuclideanColorFiltering();
+                // set center colol and radius
+                oklidfilter.CenterColor = new RGB(0, 215, 0);
+                oklidfilter.Radius = 100;
+                // apply the filter
+                oklidfilter.ApplyInPlace(image1);
+                nesnebul(image1);
+
+            }
+
+            if (radioButton3.Checked)
+            {
+                // create filter
+                EuclideanColorFiltering oklidfilter = new EuclideanColorFiltering();
+                // set center color and radius
+                oklidfilter.CenterColor = new RGB(Color.FromArgb(30, 144, 255));
+                oklidfilter.Radius = 100;
+                // apply the filter
+                oklidfilter.ApplyInPlace(image1);
+                nesnebul(image1);
+
+            }
+
+
+        }
+        public void nesnebul( Bitmap deneme)
+        { 
+            // Blob counter yaptırılması
+            BlobCounter bc = new BlobCounter();
+            bc.FilterBlobs = true;
+            bc.MinHeight = 5;
+            bc.MinWidth = 5;
+            bc.ProcessImage(deneme);
+
+            Rectangle[] rects = bc.GetObjectsRectangles();
+            
+
+
+            if (rects.Length > 0)
+            {
+
+                Rectangle objectRect = rects[0];
+                //Graphics g = Graphics.FromImage(image);
+                Graphics g = pictureBox1.CreateGraphics();
+                using (Pen pen = new Pen(Color.FromArgb(0, 0, 235), 3))
+                {
+                    g.DrawRectangle(pen, objectRect);
+                }
+                //Cizdirilen Dikdörtgenin Koordinatlari aliniyor.
+                int objectX = objectRect.X + (objectRect.Width / 2);
+                int objectY = objectRect.Y + (objectRect.Height / 2);
+
+
+                g.Dispose();
+
+                if (objectX <= 150 && objectY <= 120)
+                {
+                    serialPort1.Write("1");
+                }
+                else if (objectX > 150 && objectX < 300 && objectY <= 120)
+                {
+                    serialPort1.Write("2");
+                }
+                else if (objectX >= 300 && objectY <= 120)
+                {
+                    serialPort1.Write("3");
+                }
+                else if (objectX < 150 && objectY > 120 && objectY < 240)
+                {
+                    serialPort1.Write("4");
+                }
+                else if (objectX > 150 && objectX < 300 && objectY > 120 && objectY < 240)
+                {
+                    serialPort1.Write("5");
+                }
+                else if (objectX > 300 && objectY > 120 && objectY < 240)
+                {
+                    serialPort1.Write("6");
+                }
+                else if (objectX < 150 && objectY > 240)
+                {
+                    serialPort1.Write("7");
+                }
+                else if (objectX > 150 && objectX < 300 && objectY > 240)
+                {
+                    serialPort1.Write("8");
+                }
+                else if (objectX > 300 && objectY > 240)
+                {
+                    serialPort1.Write("9");
+                }
+
+            }
+           
+            else 
+            {
+                serialPort1.Write("a");
+            }
+            
+            pictureBox2.Image = deneme;
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // signal to stop
+            videoSource.SignalToStop();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox2.SelectedItem.ToString();
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+            if (serialPort1.IsOpen)
+            {
+                MessageBox.Show("Port Bağlantısı Yapıldı.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            MessageBox.Show("Port Bağlantısı Kesildi.");
+        }
+    }
+}
+
